a0110e654efba715c03c07b53c54c07b4a95150e
Adam Schubert <adam.schubert@sg1-game.net>
2021-10-23 05:03:38 +0200

    feat(Deps): Update for nette3

--- src/Salamek/MojeOlomouc/DI/MojeOlomoucExtension.php
+++ src/Salamek/MojeOlomouc/DI/MojeOlomoucExtension.php
@@ -2,42 +2,40 @@
 
 namespace Salamek\MojeOlomouc\DI;
 
-use Nette;
+use Nette\DI\CompilerExtension;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
+use Salamek\MojeOlomouc\NetteMojeOlomouc;
 
 
 /**
  * Class MojeOlomoucExtension
  * @package Salamek\MojeOlomouc\DI
  */
-class MojeOlomoucExtension extends Nette\DI\CompilerExtension
+class MojeOlomoucExtension extends CompilerExtension
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigSchema(): Schema
+    {
+        return Expect::structure([
+            'apiKey' => Expect::string()->required(),
+            'isProduction' => Expect::bool()->required(),
+            'hydrationTable' => Expect::array()->default([]),
+            'appendDefaultHydrationTable' => Expect::bool()->default(true)
+        ]);
+    }
 
     /**
      * {@inheritdoc}
      */
     public function loadConfiguration()
     {
-        $config = $this->getConfig();
+        $config = (array) $this->getConfig();
         $builder = $this->getContainerBuilder();
 
-
         $builder->addDefinition($this->prefix('mojeOlomouc'))
-            ->setType('Salamek\MojeOlomouc\NetteMojeOlomouc')
-            ->setArguments([$config['apiKey'], $config['isProduction'], $config['hydrationTable'], $config['appendDefaultHydrationTable']]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfig(array $defaults = [], $expand = true)
-    {
-        $defaults = [
-            'apiKey' => null,
-            'isProduction' => false,
-            'hydrationTable' => [],
-            'appendDefaultHydrationTable' => true
-        ];
-
-        return parent::getConfig($defaults, $expand);
+            ->setFactory(NetteMojeOlomouc::class, [$config['apiKey'], $config['isProduction'], $config['hydrationTable'], $config['appendDefaultHydrationTable']]);
     }
 }
